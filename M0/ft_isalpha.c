67faf01ace8b17d8387faf663aeb089b76700745
dromo <diego.romo@outlook.com>
2025-05-03 01:46:14 +0200

    Adding ft_isalpha

--- M0/ft_isalpha.c
+++ M0/ft_isalpha.c
@@ -10,4 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include <unistd.h>
 
+int	ft_isalpha(int c);
+int ft_islower(int c);
+int ft_isupper(int c);
+
+int	ft_isalpha(int c)
+{
+	if (ft_islower(c) || ft_isupper(c))
+		return (1);
+	else
+		return (0);
+}
+
+int ft_islower(int c)
+{
+    if(c >= 'a' && c <= 'z')
+        return (1);
+    else
+        return (0);
+}
+
+int ft_isupper(int c)
+{
+    if(c >= 'A' && c <= 'Z')
+        return (1);
+    else
+        return (0);
+}
+
+/*int	main(void)
+{
+	if (ft_isalpha(128))
+        write(1, "T", 1);
+    else
+        write(1, "F", 1);
+    write(1, "\n", 1);
+}*/
