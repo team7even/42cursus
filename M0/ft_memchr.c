8d844cf57e3d75e4609b5085772e141ce58bae39
DESKTOP-IV5R49C\VersusPC <diego.romo@outlook.com>
2025-05-06 21:39:36 +0200

    Adding memchr and strncmp. Updating libft and Makefile to reflect that

---
+++ M0/ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dromo-me <dromo-me@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/06 00:06:28 by dromo-me          #+#    #+#             */
+/*   Updated: 2025/05/06 00:06:31 by dromo-me         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*b;
+
+	i = 0;
+	b = (unsigned char *)s;
+	while (i < n)
+	{
+		if ((unsigned char)b[i] == (unsigned char)c)
+			return ((char *) &b[i]);
+		i++;
+	}
+	return (NULL);
+}
---
+++ M0/ft_strncmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dromo-me <dromo-me@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 23:20:06 by dromo-me          #+#    #+#             */
+/*   Updated: 2025/05/05 23:20:08 by dromo-me         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i]
+			|| s1[i] == '\0' || s2[i] == '\0')
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
