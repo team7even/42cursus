42f7f74e4b484fcd8fc8f528792a3da71197deb5
dromo-me <dromo-me@student.42malaga.com>
2025-05-19 18:45:56 +0200

    Added strjoin and updated libft and makefile

---
+++ M0/ft_strjoin.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dromo <dromo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/16 21:45:25 by dromo             #+#    #+#             */
+/*   Updated: 2025/05/19 18:44:55 by dromo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	joined = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!joined)
+		return (NULL);
+	while (i < ft_strlen(s1))
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	while (j < ft_strlen(s2))
+	{
+		joined[i + j] = s2[j];
+		j++;
+	}
+	joined[i + j] = '\0';
+	return (joined);
+}
+
+/*Reserva (con malloc(3)) y devuelve una nueva
+string, formada por la concatenación de ’s1’ y
+’s2’
+*/
--- M0/libft.h
+++ M0/libft.h
@@ -6,7 +6,7 @@
 /*   By: dromo <dromo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 23:20:08 by dromo-me          #+#    #+#             */
-/*   Updated: 2025/05/12 22:10:42 by dromo            ###   ########.fr       */
+/*   Updated: 2025/05/19 18:41:23 by dromo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
