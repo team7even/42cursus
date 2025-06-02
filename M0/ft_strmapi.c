589e143182130023d10f6b99d2070408bf11fd2b
DESKTOP-IV5R49C\VersusPC <diego.romo@outlook.com>
2025-06-02 22:53:26 +0200

    Adding ft_strmapi

---
+++ M0/ft_strmapi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dromo-me <dromo-me@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 22:46:14 by dromo-me          #+#    #+#             */
+/*   Updated: 2025/06/02 22:46:14 by dromo-me         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// Returns a new string resulting from applying function *f to each character
+// of the string *s, passing both index and char
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*res;
+	unsigned int	i;
+	unsigned int	len;
+
+	i = 0;
+	len = ft_strlen(s);
+	if (!s || !f)
+		return (NULL);
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	while (i < len)
+	{
+		res[i] = f(i, s[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
