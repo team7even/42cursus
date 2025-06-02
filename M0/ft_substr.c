228f8303aede0cd9d8e57f269e820bf9a41662d0
DESKTOP-IV5R49C\VersusPC <diego.romo@outlook.com>
2025-06-02 20:03:08 +0200

    Updated ft_substr

--- M0/ft_substr.c
+++ M0/ft_substr.c
@@ -12,29 +12,30 @@
 
 #include "libft.h"
 
+/* Reserve with malloc() and returns a substring from 's'. Substring  starts
+on 'start' index and has a max length of 'len'
+*/
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	char	*sub;
 
 	i = 0;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) <= start)
+		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
 	sub = (char *)malloc(sizeof(char) * (len + 1));
 	if (!sub)
 		return (NULL);
-	if (ft_strlen(s) <= start)
-		sub[i] = '\0';
-	else
+	while (i < len && s[start + i] != '\0')
 	{
-		while (i < len && s[start + i] != '\0')
-		{
-			sub[i] = s[start + i];
-			i++;
-		}
-		sub[len] = '\0';
+		sub[i] = s[start + i];
+		i++;
 	}
+	sub[len] = '\0';
 	return (sub);
 }
-
-/* Reserva (con malloc(3)) y devuelve una substring de la string ’s’.
-La substring empieza desde el índice ’start’ y tiene una longitud máxima ’len’
-*/
