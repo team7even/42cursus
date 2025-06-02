669cdf7b1dd44235bc5105f8a4fb5ecebc9f3a40
DESKTOP-IV5R49C\VersusPC <diego.romo@outlook.com>
2025-06-02 22:06:16 +0200

    Adding ft_split

---
+++ M0/ft_split.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dromo-me <dromo-me@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 21:33:14 by dromo-me          #+#    #+#             */
+/*   Updated: 2025/06/02 21:33:14 by dromo-me         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// Count number of words in string 's' separated by delimiter 'c'
+static int	count_words(const char *s, char c)
+{
+	int	count;
+	int	in_word;
+
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && !in_word)
+		{
+			in_word = 1;
+			count++;
+		}
+		else if (*s == c)
+			in_word = 0;
+		s++;
+	}
+	return (count);
+}
+
+// Helper: free allocated memory in case of failure
+static void	free_all(char **res, int i)
+{
+	while (i--)
+		free(res[i]);
+	free(res);
+}
+
+static int	skip_delims(const char *s, int i, char c)
+{
+	while (s[i] && s[i] == c)
+		i++;
+	return (i);
+}
+
+static int	split_fill(char **res, const char *s, char c)
+{
+	int	i;
+	int	start;
+	int	word_i;
+
+	i = 0;
+	word_i = 0;
+	while (s[i])
+	{
+		i = skip_delims(s, i, c);
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
+		{
+			res[word_i] = ft_substr(s, start, i - start);
+			if (!res[word_i])
+				return (free_all(res, word_i), 0);
+			word_i++;
+		}
+	}
+	res[word_i] = NULL;
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
+	int		words;
+
+	if (!s)
+		return (NULL);
+	words = count_words(s, c);
+	res = (char **)malloc(sizeof(char *) * (words + 1));
+	if (!res)
+		return (NULL);
+	if (!split_fill(res, s, c))
+		return (NULL);
+	return (res);
+}
